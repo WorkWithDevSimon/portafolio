e21678643a7443c328552ca662bc4abc3b87e574
Simon <simonsantibanez16@gmail.com>
2024-10-09 12:04:20 -0300

    Updates

---
+++ assets/Contacto-5c01d91e.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-07d76a70.js";import{C as t}from"./ComponenteContacto-79c3b945.js";const m=()=>o.jsx(t,{});export{m as Contacto};
--- assets/Contacto-78d40187.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./index-a8959c95.js";import{C as t}from"./ComponenteContacto-2f71638d.js";const m=()=>o.jsx(t,{});export{m as Contacto};
